bcbe7739dd17b8598aef81a3989755cb971aac17
marisasaunders <marisaksaunders@gmail.com>
2022-02-25 15:43:33 -0500

    commit tree 1511dee9572a4cf82faa43cc29509a8e8d6b2201

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ class Circle {
   }
 
   set diameter(newDiameter) {
+    this.radius = newDiameter / 2
     // this.radius = this.radius * 2
     // console.log('newDiameter', newDiameter)
   }
 
   set circumference(newCircumference) {
+    this.radius = newCircumference / (2 * Math.PI)
     // this.diameter = Math.PI * this.diameter
     // console.log('new circumference', newCircumference)
   }
 
   set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI)
     // this.radius = MATH.PI * (Math.sqrt(newArea))
     // console.log('newArea', newArea)
   }
