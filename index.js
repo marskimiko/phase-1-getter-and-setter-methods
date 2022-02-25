4679183a48ef072796822283c25dab0e3394e1b4
marisasaunders <marisaksaunders@gmail.com>
2022-02-25 15:17:47 -0500

    Done.

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ class Circle {
   }
 
   set diameter(newDiameter) {
-    this.radius = this.radius * 2
-    console.log('newDiameter', newDiameter)
+    // this.radius = this.radius * 2
+    // console.log('newDiameter', newDiameter)
   }
 
   set circumference(newCircumference) {
-    this.diameter = Math.PI * this.diameter
-    console.log('new circumference', newCircumference)
+    // this.diameter = Math.PI * this.diameter
+    // console.log('new circumference', newCircumference)
   }
 
   set area(newArea) {
-    this.radius = MATH.PI * (Math.sqrt(newArea))
-    console.log('newArea', newArea)
+    // this.radius = MATH.PI * (Math.sqrt(newArea))
+    // console.log('newArea', newArea)
   }
 }
